c9575cad471c05e5c66be4efc43af27d2822198f
Sidhant Pandey <sidhantpandeynp6@gmail.com>
2025-04-27 21:59:28 +0530

    Search in BST

---
+++ Search_BST.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean search(TreeNode node, int target) {
+        while(node != null) {
+            if (node.val == target) return true;
+            node = target < node.val ? node.left : node.right;
+        }
+        return false;
+    }
+}
